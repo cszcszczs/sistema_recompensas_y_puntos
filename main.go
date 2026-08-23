ee28c5270f93631d491027f55aadb8af9d6b3d80
cszcszczs <lasbolas01@gmail.com>
2026-08-23 02:36:37 -0500

    <feat> add endpoints

--- main.go
+++ main.go
@@ -1,19 +1,33 @@
 package main
 
 import (
+	"log"
 	"net/http"
 
+	"git.com/api-rest/handlers"
+	"git.com/api-rest/repositories"
+	"git.com/api-rest/services"
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
-	r := gin.Default()
+	repo := repositories.NewMemoryRepository()
+	service := services.NewRewardService(repo)
+	handler := handlers.NewRewardHandler(service)
 
-	r.GET("/", func(c *gin.Context) {
+	r := gin.Default()
+	v1 := r.Group("/api/v1")
+	v1.POST("/purchases", handler.RegisterPurchase)
+	v1.GET("/customers/:id/points", handler.GetPointsBalance)
+	v1.POST("/customers/:id/redeem", handler.RedeemPoints)
+
+	r.GET("/ping", func(c *gin.Context) {
 		c.JSON(http.StatusOK, gin.H{
-			"message": "Hello, API REST with gin",
+			"message": "pong",
 		})
 	})
 
-	r.Run(":8080")
+	if err := r.Run(":8080"); err != nil {
+		log.Fatalf("Error al iniciar el servidor Gin: %v", err)
+	}
 }
