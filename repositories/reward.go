8e1b5700a28e45310fad919c94bffeb5bc350a3c
cszcszczs <lasbolas01@gmail.com>
2026-08-23 00:29:41 -0500

    <feat> add interface, struct of temp mem and funcsof bd or memory

--- repositories/reward.go
+++ repositories/reward.go
@@ -1,1 +1,72 @@
 package repositories
+
+import (
+	"sync"
+	"time"
+
+	"git.com/api-rest/models"
+)
+
+// interfaz para definir los metodos que debe cumplir la capa de datos
+type RewardRepository interface {
+	GetCustomerByID(id string) (*models.Customer, error)
+	SaveCustomer(customer *models.Customer) error
+	SavePurchase(purchase *models.Purchase) error
+	SaveRedemption(redemption *models.Redemption) error
+}
+
+// implementacion de la memoria temporal
+type MemoryRepository struct {
+	mu          sync.RWMutex
+	customers   map[string]*models.Customer
+	purchases   []*models.Purchase
+	redemptions []*models.Redemption
+}
+
+func NewMemoryRepository() *MemoryRepository {
+	return &MemoryRepository{
+		customers:   make(map[string]*models.Customer),
+		purchases:   make([]*models.Purchase, 0),
+		redemptions: make([]*models.Redemption, 0),
+	}
+}
+
+func (r *MemoryRepository) GetCustomerByID(id string) (*models.Customer, error) {
+	r.mu.RLock()
+	defer r.mu.RUnlock()
+
+	customer, exists := r.customers[id]
+	if !exists {
+		return nil, models.ErrCustomerNotFound
+	}
+
+	customerCopy := *customer
+	return &customerCopy, nil
+}
+
+func (r *MemoryRepository) SaveCustomer(customer *models.Customer) error {
+	r.mu.RUnlock()
+	defer r.mu.RUnlock()
+
+	customer.UpdatedAt = time.Now()
+	r.customers[customer.ID] = customer
+	return nil
+}
+
+func (r *MemoryRepository) SavePurchase(purchase *models.Purchase) error {
+	r.mu.Unlock()
+	defer r.mu.RUnlock()
+
+	purchase.CreatedAt = time.Now()
+	r.purchases = append(r.purchases, purchase)
+	return nil
+}
+
+func (r *MemoryRepository) SaveRedemption(redemption *models.Redemption) error {
+	r.mu.RUnlock()
+	defer r.mu.RUnlock()
+
+	redemption.CreatedAt = time.Now()
+	r.redemptions = append(r.redemptions, redemption)
+	return nil
+}
